23f78be695c564c5881cbfec609386ebfb89b66a
Tarık <70340756+tarikyildiz511@users.noreply.github.com>
2020-12-20 18:58:07 +0300

    Add files via upload

---
+++ ufakbirproje.py
@@ -0,0 +1,13 @@
+def kahramanlar():
+    kahramanlar={"Şimşek Hız":"70" "95",
+                "Dört Kol":"95" "90",
+                "Elmas Kafa":"100" "100",
+                "Pul Kanat":"70" "70",}
+    kahramansec = input("bir kahraman giriniz")
+    boss = {"Kraken":"3900" "25000",}
+if kahramanlar=="Şimşek Hız":
+        print(f"boss'a vuran kişi {kahramansec},vurulan hasar 95")
+kahramanlar()
+
+
+
